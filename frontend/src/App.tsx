d083b482ff0ae7e9d6250e13544d412af81768ff
Mohammed JBILOU <95973236+Molaryy@users.noreply.github.com>
2023-12-12 00:05:39 +0100

    refactor: refactored all the css and removed all the position: absolute to add flex-box

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,13 @@
 import Navbar from './components/Navbar.tsx';
 import TimerFormHandler from './components/Form/TimerFormHandler.tsx';
-import ListTodosContainer from './components/Todos/ListTodosContainer.tsx';
+/* import ListTodosContainer from './components/Todos/ListTodosContainer.tsx'; */
 
 function App() {
-
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <TimerFormHandler />
-      <ListTodosContainer/>
+      {/* <ListTodosContainer/> */}
     </>
   );
 }
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,6 +1,14 @@
 import '../styles/NavBar.scss';
 
 const Navbar = () => {
-  return <div className={'navbar'}></div>;
+  return (
+    <div className={'navbar'}>
+      <ul>
+        <li>Home</li>
+        <li>Connect</li>
+        <li>About</li>
+      </ul>
+    </div>
+  );
 };
 export default Navbar;
